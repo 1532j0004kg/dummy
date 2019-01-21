27a41b84da275a2c1cb5d882d4d9aa484f22fc0e
dinesh <dineshozian@gmail.com>
2019-01-21 12:55:54 +0530

    yet to fini

--- js/main.js
+++ js/main.js
@@ -250,7 +250,7 @@ $(document).ready(function(){
         // If the count down is over, write some text 
         if (distance < 0) {
             clearInterval(x);
-            document.getElementById("timer").innerHTML = "On your mark";
+            document.getElementById("timer").innerHTML = "A creative overdose";
         }
     }, 1000);
 
