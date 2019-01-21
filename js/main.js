4d0fc7b18cf2a1189fdd7195a99c3a8613ca5265
dinesh <dineshozian@gmail.com>
2019-01-21 12:53:42 +0530

    yet to finished

--- js/main.js
+++ js/main.js
@@ -226,7 +226,7 @@ $(document).ready(function(){
 
 
     // Set the date we're counting down to
-    var countDownDate = new Date("feb 9, 2019 08:00:00").getTime();
+    var countDownDate = new Date("feb 09, 2019 08:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
